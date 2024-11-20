e380f0324ac95ac21f6e7799ef951640af5b6cbc
Luan Henrique <152590653+Lhenrick@users.noreply.github.com>
2024-11-19 23:48:19 -0300

    Start skills

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -2,18 +2,28 @@ import Logo from "../../components/Logo";
 import LoginForm from "../../components/LoginForm";
 import { Link } from "react-router-dom";
 
-
 export default function Login() {
+  localStorage.removeItem("authToken");
 
-    localStorage.removeItem('authToken');
-
-    return (
-        <div className="flex flex-col bg-[url('/assets/images/thunder.gif')] bg-cover bg-no-repeat items-center justify-center h-screen  bg-zinc-800">
-            <h1 className="font-bold text-white text-4xl ">Say it to the world!</h1>
-            <h2 className="font-bold text-white text-4xl animate-pulse">🌍</h2>
-            <Logo />
-            <LoginForm btText={'Login'}/>
-            <p className="mt-2 text-white">Still dont have an account? <Link to="/QuickPost/register" className=" text-yellow-400">Click here</Link></p>
-        </div>
-    )
+  return (
+    <div className="flex flex-col bg-[url('/assets/images/thunder.gif')] bg-cover bg-no-repeat items-center justify-center h-screen  bg-zinc-800">
+      <h1 className="font-bold text-white text-4xl ">Say it to the world!</h1>
+      <h2 className="font-bold text-white text-4xl animate-pulse">🌍</h2>
+      <Logo />
+      <LoginForm btText={"Login"} />
+      <p className="mt-2 text-white">
+        Still dont have an account?{" "}
+        <Link to="/QuickPost/register" className=" text-yellow-400">
+          Click here
+        </Link>
+      </p>
+      <p className="mt-2 text-white">OR</p>
+      <p className="mt-2 text-white">
+        Enter as a guest.{" "}
+        <Link to="/QuickPost/register" className=" text-yellow-400">
+          Click here
+        </Link>
+      </p>
+    </div>
+  );
 }
--- src/components/profileInfo/index.jsx
+++ src/components/profileInfo/index.jsx
@@ -1,63 +1,67 @@
-import { useEffect, useState } from "react"
-import { fetchProfile } from "../../Services/userServices"
+import { useEffect, useState } from "react";
+import { fetchProfile } from "../../Services/userServices";
 import { userPosts } from "../../Services/postServices";
 import Post from "../Post";
-
+import Skills from "../Skills";
 
 //Get user data from the 'UserService'
 export default function ProfileInfo() {
+  const [user, setUser] = useState("");
+  const [post, setPost] = useState([]);
 
-    const [user, setUser] = useState('');
-    const [post, setPost] = useState([]);
+  useEffect(() => {
+    async function getUserData() {
+      const userData = await fetchProfile();
+      setUser(userData);
+    }
+    getUserData();
+  }, []);
 
-    useEffect(() => {
-        async function getUserData() {
-            const userData = await fetchProfile();
-            setUser(userData);
-        }
-        getUserData();
-    }, [])
+  //print all the posts of the logged in user
 
-    //print all the posts of the logged in user
+  useEffect(() => {
+    async function fetchPosts(userId) {
+      const posts = await userPosts(userId);
+      const sortedPosts = posts.reverse();
+      setPost(sortedPosts);
+    }
+    if (user._id) {
+      fetchPosts(user._id);
+    }
+  }, [user._id]);
 
-    useEffect(() => {
-        async function fetchPosts(userId) {
-            const posts = await userPosts(userId);
-            const sortedPosts = posts.reverse();
-            setPost(sortedPosts);
-        }
-        if (user._id) {
-            fetchPosts(user._id)
-        }
+  //Display profile info like name, bio and picture.
+  return (
+    <section className="flex flex-col pb-5 bg-gradient-to-br from-yellow-300 via-neutral-800 to-neutral-900  min-h-screen h-auto items-center">
+      <div className="flex flex-col items-center p-5 h-full w-full border-2 border-black bg-[url('/assets/images/cloudbg.jpg')]  ">
+        <div className="size-56 sm:size-72">
+          <img
+            src="./quick.jpeg"
+            loading="lazy"
+            className=" rounded-full border-4 border-yellow-400"
+          />
+        </div>
+        <h2 className="text-white font-extrabold text-5xl">@{user.username}</h2>
+        <p className="text-white font-semibold">{user.email}</p>
+      </div>
+      <Skills />
+      <section className="mt-10 size-5/6 md:size-3/6">
+        {post.length == 0 ? (
+          <h3 className="text-white text-center mb-3 font-bold text-2xl sm:text-4xl">
+            You haven't posted anything yet 😒
+          </h3>
+        ) : (
+          <h3 className="text-white text-center mb-3 font-bold text-2xl sm:text-4xl">
+            Your QuickPosts
+          </h3>
+        )}
 
-    }, [user._id]);
-
-    //Display profile info like name, bio and picture.
-    return (
-        <section className="flex flex-col pb-5 bg-gradient-to-br from-yellow-300 via-neutral-800 to-neutral-900  min-h-screen h-auto items-center">
-            <div className="flex flex-col items-center p-5 h-full w-full border-2 border-black bg-[url('/assets/images/cloudbg.jpg')]  ">
-                <div className="size-56 sm:size-72" >
-                    <img src="./quick.jpeg" loading="lazy" className=" rounded-full border-4 border-yellow-400" />
-                </div>
-            <h2 className="text-white font-extrabold text-5xl">
-                @{user.username}
-            </h2>
-            <p className="text-white font-semibold">
-                {user.email}
-            </p>
-            </div>
-            <section className="mt-10 size-5/6 md:size-3/6">               
-                    {post.length == 0 ? <h3 className="text-white text-center mb-3 font-bold text-2xl sm:text-4xl">You haven't posted anything yet 😒</h3> :
-                    <h3 className="text-white text-center mb-3 font-bold text-2xl sm:text-4xl">Your QuickPosts</h3> }
-
-                <div className="flex flex-col gap-5 ">
-                    {post.map((userPost) => (
-                        <Post key={userPost.title} data={userPost} />
-                    ))}
-                    
-                </div>
-            </section>
-
-        </section>
-    )
+        <div className="flex flex-col gap-5 ">
+          {post.map((userPost) => (
+            <Post key={userPost.title} data={userPost} />
+          ))}
+        </div>
+      </section>
+    </section>
+  );
 }
